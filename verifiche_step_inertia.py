761d5345b54e67be1f24465c50da3de2cdfd9687
Alessio Brunamonti <alessio.brunamonti@mail.polimi.it>
2024-03-12 12:44:47 +0100

    confrontata presenza dei generatori GFM

--- verifiche_step_inertia.py
+++ verifiche_step_inertia.py
@@ -9,12 +9,12 @@ original_inertia = {'G 02': 4.33,
                     'G 08': 3.47,
                     'G 09': 3.45,
                     'G 10': 4.20}
-config_file = 'config\\IEEE39_AC_config.json'
+config_file = 'config\\IEEE39_AC_config_grid_former.json'
 config = json.load(open(config_file, 'r'))
 for i in range(2,10):
     config['synch_mach']['G 0' + str(i)] = {'typ_id.h' : original_inertia['G 0' + str(i)]}
 config['synch_mach']['G 10'] = {'typ_id.h' : original_inertia['G 10']}
 
-config['outdir'] = 'C:\\Users\\aless\\Desktop\\inertia step simulations\\verifiche'
-out_config_file = 'C:\\Users\\aless\\Desktop\\inertia step simulations\\config_verifiche.json'
+config['outdir'] = 'C:\\Users\\aless\\Desktop\\inertia step simulations\\verifiche_2'
+out_config_file = 'C:\\Users\\aless\\Desktop\\inertia step simulations\\config_verifiche_2.json'
 json.dump(config, open(out_config_file, 'w'), indent = 4)
