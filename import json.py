cfb7f77648e3be5b54d204b438a481352a1a5d99
Alessio Brunamonti <alessio.brunamonti@mail.polimi.it>
2024-04-02 13:16:01 +0200

    fatto modifiche pesanti preparati

--- import json.py
+++ import json.py
@@ -1,6 +1,6 @@
 import json
 import numpy as np
 
-data = np.load('C:\\Users\\aless\\Desktop\\inertia step simulations\\no_step\\_3_3_3_2\\IEEE 39 fake grid forming_AC.npz', allow_pickle = True)
+data = np.load('C:\\Users\\aless\\Desktop\\inertia step simulations\\no_step\\_3_3_3_699\\IEEE 39 fake grid forming_AC_TF_-6.0_2.0_100.npz', allow_pickle = True)
 
-print(data['config'])
+print(data['TF'].shape)
