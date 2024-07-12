985148398a50ba8158eee4f2150302f9bdcf8097
Alessio Brunamonti <alessio.brunamonti@mail.polimi.it>
2024-07-12 13:28:19 +0200

    problematic selection of spectrum resolved

--- compute_TFs_extended.py
+++ compute_TFs_extended.py
@@ -18,10 +18,10 @@ def usage(exit_code=None):
 
 
 if __name__ == '__main__':
-    directory = "D:\\POLIMI\\AI stable power\\39 New England\\39 line simu"
-    outdir = "D:\\POLIMI\\AI stable power\\39 New England\\spettri no step"
+    directory = "C:\\Users\\aless\\Desktop\\dataset_AI_stablepower\\small_experiments\\confronto_spettri_gen_OOS\\simu_output"
+    outdir = "C:\\Users\\aless\\Desktop\\dataset_AI_stablepower\\small_experiments\\confronto_spettri_gen_OOS\\spettri"
     list_simulations = os.listdir(directory) #crea una lista di tutte le simulazioni
-    file_jacobiano = 'IEEE 39 fake grid forming line_AC.npz' #tutti i file hanno questo nome
+    file_jacobiano = 'IEEE39_stoch_CIG_AC.npz' #tutti i file hanno questo nome
 
     # default values    
     fmin,fmax = -3., -1. #tronchiamo a 0.1Hz
@@ -227,7 +227,7 @@ if __name__ == '__main__':
         interesting_variables = ['G 01.speed', 'G 02.speed', 'G 03.speed', 
                                  'G 04.speed', 'G 05.speed', 'G 06.speed', 
                                  'G 07.speed', 'G 08.speed', 'G 09.speed', 'G 10.speed']
-        index = np.ones(TF.shape[1], dtype=bool)
+        index = []
         var_to_save = []
         for var in var_names:
             if var in interesting_variables:
@@ -238,8 +238,7 @@ if __name__ == '__main__':
         print(len(var_names))
         print(len(index))
         print(TF.shape)
-        TF = np.squeeze(TF)[:, :index.size]
-        
+        TF = TF[:,:,index]
         out = {'A': A, 'F': F, 'TF': TF,
             'var_names': var_to_save, 'SM_names': SM_names, 'bus_names': bus_names,
             'Htot': Htot, 'Etot': Etot, 'Mtot': Mtot, 'H': H, 'S': S, 'P': P, 'Q': Q,
